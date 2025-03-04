13975d9367785aadb4cbb73753a226445a534502
Daniel Wu <danielwu.toucan@gmail.com>
2025-03-04 04:29:30 -0500

    [Fix] Clarifications to titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,70 +20,67 @@ export default function Home() {
         <div className="flex items-end">
           <Dropdown
             options={options}
-            title="(1) Optional Single Select"
+            title="Optional Single Select"
             multiSelect={false}
             onChange={(selected) => setValue1(selected)}
             value={value1}
           />
           <div className="ps-10">
-            Value of (1): {value1 !== null ? value1 : "None"}{" "}
+            Value: {value1 !== null ? value1 : "None"}{" "}
           </div>
         </div>
         <div className="h-8" />
         <div className="flex items-end">
           <Dropdown
             options={options}
-            title="(2) Optional Multi Select"
+            title="Optional Multi Select"
             multiSelect={true}
             onChange={(selected) => setValue2(selected)}
             value={value2}
           />
           <div className="ps-10">
-            Value of (2):{" "}
-            {value2 && value2.length > 0 ? value2.join(", ") : "None"}{" "}
+            Value: {value2 && value2.length > 0 ? value2.join(", ") : "None"}{" "}
           </div>
         </div>
         <div className="h-8" />
         <div className="flex items-end">
           <Dropdown
             options={options}
-            title="(3) Required Single Select"
+            title="Required Single Select"
             multiSelect={false}
             required={true}
             onChange={(selected) => setValue3(selected)}
             value={value3}
           />
           <div className="ps-10">
-            Value of (3): {value3 !== null ? value3 : "None"}{" "}
+            Value: {value3 !== null ? value3 : "None"}{" "}
           </div>
         </div>
         <div className="h-8" />
         <div className="flex items-end">
           <Dropdown
             options={options}
-            title="(4) Required Multi Select"
+            title="Required Multi Select"
             multiSelect={true}
             required={true}
             onChange={(selected) => setValue4(selected)}
             value={value4}
           />
           <div className="ps-10">
-            Value of (4):{" "}
-            {value4 && value4.length > 0 ? value4.join(", ") : "None"}{" "}
+            Value: {value4 && value4.length > 0 ? value4.join(", ") : "None"}{" "}
           </div>
         </div>
         <div className="h-8" />
         <div className="flex items-end">
           <Dropdown
             options={manyOptions}
-            title="(5) Optional Many Items Multi Select"
+            title="So many options..."
             multiSelect={true}
             onChange={(selected) => setValue5(selected)}
             value={value5}
           />
           <div className="ps-10">
-            Value of (5):{" "}
-            {value5 && value5.length > 0 ? value5.join(", ") : "None"}{" "}
+            Value: {value5 && value5.length > 0 ? value5.join(", ") : "None"}{" "}
           </div>
         </div>
       </div>
